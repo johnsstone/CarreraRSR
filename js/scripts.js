54fc84ef898c4f74d20e641db2a473c3e189ff88
John Stone <johnsstone@gmail.com>
2018-03-21 20:10:37 -0400

    update from week 11

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/* JavaScript */
+
+/* Send email or comments */
+
+  function sendMail() {
+    var link = 'mailto:hello@domain.com?subject=Message from '
+             +document.getElementById('email_address').value
+             +'&body='+document.getElementById('email_address').value;
+    window.location.href = link;
+    console.log(link);
+}
+
+
+
